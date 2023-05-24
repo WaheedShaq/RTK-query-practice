7ec21a111bac10ecff53ee6ea554d716e2bc5261
WaheedShaq <waheedshaqra@gmail.com>
2023-05-24 08:10:04 +0300

    used axios to fetch data from my API and added a skeleton loader for the loading component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import './index.css';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css';
 
 const el = document.getElementById('root');
 const root = createRoot(el);
