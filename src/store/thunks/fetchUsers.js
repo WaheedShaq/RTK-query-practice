867107a4950d574c2f1d1db65c6c2576310920ca
WaheedShaq <waheedshaqra@gmail.com>
2023-05-29 08:49:09 +0300

    added some thunk functions for practice, and an additional custom thunk hook that manages state in the components better, added some styling on the add user button (spinner) created a functionality where the user cannot press the button until a request is done

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const response = await axios.get('http://localhost:3005/users');
+  // respsone is the action.payload property (keep that in mind)
 
-  //DEV ONLY
-  await pause(10000);
+  // DEV ONLY
+  await pause(1000);
 
   return response.data;
 });
