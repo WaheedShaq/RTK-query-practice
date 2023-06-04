cd76ee09ebefaa26065909891120d26c72312378
WaheedShaq <waheedshaqra@gmail.com>
2023-06-04 06:51:38 +0300

    added some styling and deleting functionality

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -34,7 +34,20 @@ const usersSlice = createSlice({
       state.isLoading = false;
       state.error = action.error;
     });
-    builder.addCase(removeUser.pending, (state, action) => {});
+    builder.addCase(removeUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(removeUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = state.data.filter((user) => {
+        return user.id !== action.payload.id;
+      });
+    });
+
+    builder.addCase(removeUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
   },
 });
 
