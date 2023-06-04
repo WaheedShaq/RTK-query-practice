9e45ce00a7972ebd8a8e36febe7e0590c05ddb68
WaheedShaq <waheedshaqra@gmail.com>
2023-06-04 07:19:32 +0300

    added an expandable panel component to expand/detract albums and view content within

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -2,6 +2,7 @@ import { GoTrashcan } from 'react-icons/go';
 import Button from './Button';
 import { removeUser } from '../store';
 import { useThunk } from '../hooks/useThunk';
+import ExpandablePanel from './ExpandablePanel';
 
 function UsersListItem({ user }) {
   const [doRemoveUser, isLoadingDeleteUser, removeUserError] =
@@ -10,23 +11,21 @@ function UsersListItem({ user }) {
   const handleClick = () => {
     doRemoveUser(user);
   };
-  return (
-    <div key={user.id} className='mb-2 border rounded'>
-      <div className='flex p-2 justify-between items-center cursor-pointer'>
-        <div className='flex flex-row items-center justify-between'>
-          <Button
-            className='mr-3'
-            loading={isLoadingDeleteUser}
-            onClick={handleClick}
-          >
-            <GoTrashcan />
-          </Button>
-          {removeUserError && <div>Error deleting user</div>}
-          {user.name}
-        </div>
-      </div>
-    </div>
+  const header = (
+    <>
+      <Button
+        className='mr-3'
+        loading={isLoadingDeleteUser}
+        onClick={handleClick}
+      >
+        <GoTrashcan />
+      </Button>
+      {removeUserError && <div>Error deleting user</div>}
+      {user.name}
+    </>
   );
+
+  return <ExpandablePanel header={header}>Content!!</ExpandablePanel>;
 }
 
 export default UsersListItem;
